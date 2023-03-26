6d098cfc511117c39c20af6bb06cda3f80faa21a
jbeans0312 <jbeans@udel.edu>
2023-03-26 16:33:00 -0400

    Restructured file tree

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function App() {
+    return (
+        <div className="App" style={{minHeight: '100vh'}}>
+            
+        </div>
+    );
+}
+
+export default App;
