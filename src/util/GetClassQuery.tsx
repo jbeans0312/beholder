fa1cd3cd395ee2352b0b44f07fa73cab1bacb069
jbeans0312 <jbeans@udel.edu>
2023-03-28 14:17:27 -0400

    honeslty forgot

--- src/util/GetClassQuery.tsx
+++ src/util/GetClassQuery.tsx
@@ -17,11 +17,11 @@ export const QUERY_CLASS_BY_INDEX = gql`
 export function GetClassProficiencies(name: string) {
     const {loading, error, data} = useQuery(QUERY_CLASS_BY_INDEX, {variables: {name}});
 
+
     if(loading) return 'Fetching proficiencies...';
     if (error) return `Error! ${error.message}`;
 
     return(
-        
         <>
             <ol>
                 {data.classes.map((entry: Class) => entry.proficiencies.map((prof: Proficiency)=>
