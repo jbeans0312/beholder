35bcaf1b1ba881b8099ecfdfda0938f5f80cdb0b
jbeans0312 <jbeans@udel.edu>
2023-04-20 18:53:23 -0400

    added homepage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import { Character } from '../interfaces/character';
 import { Card } from './card/Card';
+import image from './eyecon.png';
 
 function App() {
+    const [pastHome, clickPastHome] = useState(false);
+    
     const testCharacter: Character = {
         id: 0,
         name: 'Testerson Testbane',
@@ -28,28 +31,53 @@ function App() {
         armor: 'chain-mail'
     };
 
+    const [party, editParty] = useState(Array<Character>(testCharacter));
+
     return (
         <div className='container' style={{minHeight: '100vh'}}>
             <div className="App">
-                <div className="row" style={{minHeight: '35vh', display: 'flex', alignContent: 'center', flexDirection: 'column'}}>
-                    <h1 className="display-1 mt-5" style={{textDecoration: 'italic'}}>Beholder</h1>
+                {!pastHome && <div className="row" 
+                    style={{minHeight: '100vh', backgroundColor: '#FFC3A1',display: 'flex', alignContent: 'center', 
+                        justifyContent: 'center', flexDirection: 'column'}}
+                    onClick={()=>{
+                        clickPastHome(true);
+                    }}>
+                    <div className='headerContainer'>
+                        <img className='headerImage mb-3'src={image}></img> 
+                        <h1 className="display-1" style={{textDecoration: 'italic', position: 'absolute'}}>Beholder</h1>   
+                    </div>
                     <strong>
                         <p>
                             party creator and analysis tool for dnd 5e
+                        </p>
+                        <p >
+                            built on <a href='https://www.dnd5eapi.co/' target='_blank' rel="noreferrer">dnd5eapi.co</a>
+                        </p>
+                        <p>
+                            ...click to continue
+                        </p>
+                    </strong>
+                </div>}
+                {pastHome && <div className="row" style={{minHeight: '80vh'}}>
+                    <div className="col" style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center'}}>
+                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap',
+                            gap: 'space-evenly'}}>
+                            {party.map((c: Character) => <Card key={c.name} {...c}></Card>)}
+                        </div>
+                    </div>
+                </div>}
+                {pastHome && <div className="row" style={{minHeight: '20vh'}}>
+                    <strong>
+                        <p className="hoverable" onClick={()=>{
+                            clickPastHome(false);
+                        }}>
+                            back to home...
                         </p>
                         <p>
                             built on <a href='https://www.dnd5eapi.co/' target='_blank' rel="noreferrer">dnd5eapi.co</a>
                         </p>
                     </strong>
-                </div>
-                <div className="row" style={{minHeight: '95vh'}}>
-                    <div className="col" style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap',
-                            gap: 'space-evenly'}}>
-                            <Card {...testCharacter}></Card>
-                        </div>
-                    </div>
-                </div>
+                </div>}
             </div>
         </div>
     );
--- src/components/card/StatsPanel.tsx
+++ src/components/card/StatsPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Character, AbilityScore } from '../../interfaces/character';
 import './css/StatsPanel.css';
 
