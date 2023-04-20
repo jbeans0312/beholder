51a8c3d987d694fb0f5be0df86f496c237770565
jbeans0312 <jbeans@udel.edu>
2023-04-20 18:53:41 -0400

    added homepage

---
+++ src/components/NewCharButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Character } from '../interfaces/character';
+
+export function NewCharButton({party, editParty}: 
+    {party: Array<Character>, editParty: (newParty: Array<Character>) => void}):
+    JSX.Element{
+    
+    return(
+        <></>
+    );
+}
